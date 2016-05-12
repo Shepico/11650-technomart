91d98840b2bd5c51f724bbb7e2bdd8d910cab02d
Shepico <Shepico@gmail.com>
2016-05-12 22:10:19 +0400

    final #2 clear
    по боролись с картинками

--- js/feedback.js
+++ js/feedback.js
@@ -1,86 +1,87 @@
 //Всплывающее окно
-      var btnFeedbackOpen = document.querySelector(".contact-but");
-      var btnFeedbackClose = document.querySelector(".btn-feedback");      
-      var formFeedback = document.querySelector(".feedback");
-      
-      var f_name = document.querySelector("[id=name-field]");
-      var f_email = document.querySelector("[id=email-field]");
-      var f_content = document.querySelector("[id=content-field]");
-      
-      var storage_name = localStorage.getItem("f_name");
-      var storage_email = localStorage.getItem("f_email");
-      
-      btnFeedbackOpen.addEventListener("click", function(event){
-          event.preventDefault();
-          formFeedback.classList.toggle("not-visible");
-          if (storage_name) {
-            f_name.value = storage_name;          
-          }
-          
-          if (storage_email) {
-            f_email.value = storage_email;
-          }        
-          f_content.focus();
-          f_content.value = "";
-        });
-      
-        btnFeedbackClose.addEventListener("click", function(event){
-          event.preventDefault();
-          formFeedback.classList.add("not-visible");
-        });
-      
-        formFeedback.addEventListener("submit", function(event){
-          event.preventDefault();
-          if (!f_name.value) {
-            f_name.classList.add("error-fields");
-          } else{
-            if (f_name.classList.contains("error-fields")) {
-              f_name.classList.remove("error-fields");
-            }          
-            localStorage.setItem("f_name", f_name.value);
-          }  
-          
-          if (!f_email.value) {
-            f_email.classList.add("error-fields");
-          } else{
-            if (f_email.classList.contains("error-fields")) {
-              f_email.classList.remove("error-fields");
-            }          
-            localStorage.setItem("f_email", f_email.value);
-          }  
-          
-          if (!f_content.value) {
-            f_content.classList.add("error-fields");
-          } else{
-            if (f_content.classList.contains("error-fields")) {
-              f_content.classList.remove("error-fields");
-            }                    
-          }  
-        });
-        
-        window.addEventListener("keydown", function(event){
-          if (event.keyCode === 27) {
-            formFeedback.classList.add("not-visible");
-          }
-        });
-        //Большая карта
-        var btnBigMap = document.querySelector(".open-bigmap");
-        var panelBigMap = document.querySelector(".map-big");
-        
-        var btnBigMapClose = document.querySelector(".btn-map");      
-        
-        btnBigMap.addEventListener("click", function(event){
-          event.preventDefault();
-          panelBigMap.classList.toggle("not-visible");
-        });
-        
-        btnBigMapClose.addEventListener("click", function(event){
-          event.preventDefault();
-          panelBigMap.classList.add("not-visible");
-        });
-        
-        window.addEventListener("keydown", function(event){
-          if (event.keyCode === 27) {
-            panelBigMap.classList.add("not-visible");
-          }
-        });
+var btnFeedbackOpen = document.querySelector(".contact-but");
+var btnFeedbackClose = document.querySelector(".btn-feedback");      
+var formFeedback = document.querySelector(".feedback");
+
+var f_name = document.querySelector("[id=name-field]");
+var f_email = document.querySelector("[id=email-field]");
+var f_content = document.querySelector("[id=content-field]");
+
+var storage_name = localStorage.getItem("f_name");
+var storage_email = localStorage.getItem("f_email");
+
+btnFeedbackOpen.addEventListener("click", function(event){
+  event.preventDefault();
+  formFeedback.classList.toggle("not-visible");
+  formFeedback.classList.add("flash");
+  if (storage_name) {
+    f_name.value = storage_name;          
+  }
+
+  if (storage_email) {
+    f_email.value = storage_email;
+  }        
+  f_content.focus();
+  f_content.value = "";
+});
+
+btnFeedbackClose.addEventListener("click", function(event){
+  event.preventDefault();
+  formFeedback.classList.add("not-visible");
+});
+
+formFeedback.addEventListener("submit", function(event){
+  event.preventDefault();
+  if (!f_name.value) {
+    f_name.classList.add("error-fields");
+  } else{
+      if (f_name.classList.contains("error-fields")) {
+        f_name.classList.remove("error-fields");
+      }          
+      localStorage.setItem("f_name", f_name.value);
+  }  
+
+  if (!f_email.value) {
+    f_email.classList.add("error-fields");
+  } else{
+      if (f_email.classList.contains("error-fields")) {
+        f_email.classList.remove("error-fields");
+      }          
+      localStorage.setItem("f_email", f_email.value);
+    }  
+
+  if (!f_content.value) {
+    f_content.classList.add("error-fields");
+  } else{
+      if (f_content.classList.contains("error-fields")) {
+        f_content.classList.remove("error-fields");
+      }                    
+    }  
+});
+
+window.addEventListener("keydown", function(event){
+  if (event.keyCode === 27) {
+    formFeedback.classList.add("not-visible");
+  }
+});
+
+//Большая карта
+var btnBigMap = document.querySelector(".open-bigmap");
+var panelBigMap = document.querySelector(".map-big");
+var btnBigMapClose = document.querySelector(".btn-map");      
+
+btnBigMap.addEventListener("click", function(event){
+  event.preventDefault();
+  panelBigMap.classList.toggle("not-visible");
+});
+
+btnBigMapClose.addEventListener("click", function(event){
+  event.preventDefault();
+  panelBigMap.classList.add("not-visible");
+});
+
+window.addEventListener("keydown", function(event){
+  if (event.keyCode === 27) {
+    panelBigMap.classList.add("not-visible");
+  }
+});
--- js/script.js
+++ js/script.js
@@ -1,62 +1,59 @@
-      //Вход в кабинет пользователя
-      var btnLoginEnter = document.querySelector(".lp-login-enter");
-      var btnLoginExit = document.querySelector(".icon-logout");
-      var panelLogin = document.querySelector(".lp-login-registr");
-      var panelUser = document.querySelector(".lp-login-user-panel");
-      
-      btnLoginEnter.addEventListener("click", function (event) {
-        event.preventDefault();
-        panelLogin.classList.toggle("not-visible");
-        panelUser.classList.toggle("not-visible");
-      });
-      
-      btnLoginExit.addEventListener("click", function (event) {
-        event.preventDefault();
-        panelLogin.classList.toggle("not-visible");
-        panelUser.classList.toggle("not-visible");
-      });
-           
-      //Окно покупки
-      var btnBuy = document.querySelectorAll(".item-actions-buy");
-      var WindowAddCart = document.querySelector(".add-cart-window");
-      var btnWindowAddCartClose = document.querySelector(".btn-add-cart-close"); 
-      var PanelBasket = document.querySelector(".iu-basket");
-      
-      
-      for (var i = 0; i < btnBuy.length; i++) { 
-        btnBuy[i].addEventListener("click", function (event) {
-          event.preventDefault();
-          WindowAddCart.classList.toggle("not-visible");           
-          if (PanelBasket.classList.contains) {
-            PanelBasket.classList.add("background-red");
-          }
-          
-        });    
-      } ;
-           
-      btnWindowAddCartClose.addEventListener("click", function(event){
-        event.preventDefault();
-        WindowAddCart.classList.add("not-visible");        
-      });
-      
-      window.addEventListener("keydown", function(event){
-        if (event.keyCode === 27) {
-          WindowAddCart.classList.add("not-visible");
-        }
-      });
-            
-      //Закладки Красный пункт
-      var btnBookmark = document.querySelectorAll(".item-actions-bookmark");
-      var PanelBookmark = document.querySelector(".iu-marker");
-      
-      
-      for (var i = 0; i < btnBookmark.length; i++) { 
-        btnBookmark[i].addEventListener("click", function (event) {
-          event.preventDefault();
-          if (PanelBookmark.classList.contains) {
-            PanelBookmark.classList.add("background-red");
-          }
-          
-        });    
-      } ;    
-            
+//Вход в кабинет пользователя
+var btnLoginEnter = document.querySelector(".lp-login-enter");
+var btnLoginExit = document.querySelector(".icon-logout");
+var panelLogin = document.querySelector(".lp-login-registr");
+var panelUser = document.querySelector(".lp-login-user-panel");
+
+btnLoginEnter.addEventListener("click", function (event) {
+  event.preventDefault();
+  panelLogin.classList.toggle("not-visible");
+  panelUser.classList.toggle("not-visible");
+});
+
+btnLoginExit.addEventListener("click", function (event) {
+  event.preventDefault();
+  panelLogin.classList.toggle("not-visible");
+  panelUser.classList.toggle("not-visible");
+});
+
+//Окно покупки
+var btnBuy = document.querySelectorAll(".item-actions-buy");
+var WindowAddCart = document.querySelector(".add-cart-window");
+var btnWindowAddCartClose = document.querySelector(".btn-add-cart-close"); 
+var PanelBasket = document.querySelector(".iu-basket");
+
+
+for (var i = 0; i < btnBuy.length; i++) { 
+  btnBuy[i].addEventListener("click", function (event) {
+  event.preventDefault();
+  WindowAddCart.classList.toggle("not-visible");           
+  if (PanelBasket.classList.contains) {
+    PanelBasket.classList.add("background-red");
+  }
+});    
+} ;
+
+btnWindowAddCartClose.addEventListener("click", function(event){
+  event.preventDefault();
+  WindowAddCart.classList.add("not-visible");        
+});
+
+window.addEventListener("keydown", function(event){
+  if (event.keyCode === 27) {
+    WindowAddCart.classList.add("not-visible");
+  }
+});
+
+//Закладки Красный пункт
+var btnBookmark = document.querySelectorAll(".item-actions-bookmark");
+var PanelBookmark = document.querySelector(".iu-marker");
+
+
+for (var i = 0; i < btnBookmark.length; i++) { 
+  btnBookmark[i].addEventListener("click", function (event) {
+  event.preventDefault();
+  if (PanelBookmark.classList.contains) {
+    PanelBookmark.classList.add("background-red");
+  }
+});    
+} ;    
